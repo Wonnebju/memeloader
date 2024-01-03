d08b25608df8eaa0b173f7c24f5e391724778c0c
wonnebju <wonnebju@gmx.de>
2024-01-03 19:53:06 +0100

    added dotenv; added tqdm progressbar; fixed regex;

--- README.md
+++ README.md
@@ -6,6 +6,19 @@ Detects possible duplicate files with (1), (2), ... in the name and moves them t
 Uploaded files will be moved to uploaded/, where also the log.txt file will reside by default.
 These directories and the log.txt will be created if not existing already.
 
+Configuration is done in the .env, which will be created on first run in the same directory as memeloader.py.
+```dotenv
+# environment variables for memeloader.py
+
+URL=https://some.website/api_upload.php # target api url
+ID=1 # user id
+TOKEN=some_auth_token # auth token
+FILE_DESCRIPTOR=file # name of the files field the target api will use
+PREFIXES=xD_,insta_,reddit_ # file prefixes to scan for
+TYPES=jpeg,jpg,png,gif,webm,mp4,webp # file types to scan for
+UPLOAD_DIR=uploaded/ # relative directory to move files to after upload
+DUPLICATE_DIR=duplicate/ # relative directory to move duplicates to
+```
 ```python
 prefixes = ("", "")  # use this tuple to "disable" prefix condition
 types = ["jpeg"] # at least one type is required
@@ -16,6 +29,8 @@ Required packages:
   -re
   -requests
   -datetime
+  -dotenv
+  -tqdm
 ```
 Added Userscript (Greasemonkey/Tampermonkey) that complements the memeloader.
 It enables middleMouseClick to download on img and video tagged elements.
--- memeloader.py
+++ memeloader.py
@@ -2,6 +2,8 @@ import os
 import requests
 import datetime
 import re
+from dotenv import load_dotenv
+from tqdm import tqdm
 
 
 class Log:
@@ -17,7 +19,6 @@ class Log:
                 stamp = datetime.datetime.now()
                 line = stamp.strftime(self.time_format) + ": " + line
             log_file.write(line + "\n")
-            print(line)
 
 
 def scan_dir(path: str, prefixes: tuple, types: list) -> list:
@@ -41,7 +42,7 @@ def scan_dir(path: str, prefixes: tuple, types: list) -> list:
 def clean_name(name: str) -> str:
     """Clean filename of non word chars and substitute them with "_"
     """
-    name_cleaned = re.sub('[^\w-]', "_", name[:name.rfind(".")]) + name[name.rfind("."):]
+    name_cleaned = re.sub(r'[^\w\-()]', "_", name[:name.rfind(".")]) + name[name.rfind("."):]
     return name_cleaned
 
 
@@ -74,7 +75,7 @@ def split_duplicates(files: list) -> tuple:
     """
     files_cleaned, duplicates = list(), list()
     for file in files:
-        if re.search("( \(\d\)\.)", file):
+        if re.search(r"\(\d+\)", file):
             duplicates.append(file)
         else:
             files_cleaned.append(file)
@@ -103,7 +104,7 @@ def upload_move_files(files: list, url: str, data: dict, file_descriptor: str, t
     :param file_descriptor: field name for files
     :param target_dir: target move directory after upload
     """
-    for file in files:
+    for file in tqdm(files, desc="Uploading"):
         file_clean = clean_name(file)
         move_file(file, file_clean)
         with open(file_clean, "rb") as outfile:
@@ -117,24 +118,54 @@ def clean_move_files(files: list, target_dir: str):
     :param files: list of file names
     :param target_dir:
     """
-    for file in files:
+    for file in tqdm(files, desc="Cleaning"):
         file_clean = clean_name(file)
         move_file(file, target_dir + file_clean)
 
 
 if __name__ == "__main__":
-    # request data
-    url = "https://some.website/api_upload.php"  # target api url
-    data = {"id": "1",  # body data the request will use
-            "token": "some_auth_token"}
-    file_descriptor = "file"  # name of the files field the target api will use
-    prefixes = ("xD_", "animu_", "deviant_", "insta_", "reddit_")  # file prefixes to scan for
-    types = ["jpeg", "jpg", "png", "gif", "webm", "mp4", "webp"]  # file types to scan for
-    uploaded_dir = "uploaded/"  # relative directory to move files to after upload
-    duplicate_dir = "duplicate/"  # relative directory to move duplicates to
 
-    # Do not change below
+    # defaults
+    default_values = {
+        "URL": "https://some.website/api_upload.php",
+        "ID": "1",
+        "TOKEN": "some_auth_token",
+        "FILE_DESCRIPTOR": "file",
+        "PREFIXES": "xD_,insta_,reddit_",
+        "TYPES": "jpeg,jpg,png,gif,webm,mp4,webp",
+        "UPLOAD_DIR": "uploaded/",
+        "DUPLICATE_DIR": "duplicate/",
+    }
+
+    # Write the default values to a .env file if it doesn't exist
+    env_file_path = ".env"
+    if not os.path.exists(env_file_path):
+        with open(env_file_path, "w") as env_file:
+            env_file.write(f"# environment variables for memeloader.py\n\n")
+            for key, value in default_values.items():
+                env_file.write(f"{key}={value}\n")
+        print("Created .env with default values. Please configure before rerunning.")
+        exit()
+
+    load_dotenv()
+
+    # request data
+    url = os.environ.get("URL")  # target api url
+    data = {"id": os.environ.get("ID"),  # body data the request will use. Adjust this to fit your need.
+            "token": os.environ.get("TOKEN")}
+    file_descriptor = os.environ.get("FILE_DESCRIPTOR")  # name of the files field the target api will use
+
+    # file attributes to scan for and move to
+    prefixes = tuple(os.environ.get("PREFIXES").split(','))  # file prefixes to scan for
+    types = os.environ.get("TYPES").split(',')  # file types to scan for
+    uploaded_dir = os.environ.get("UPLOAD_DIR")  # relative directory to move files to after upload
+    duplicate_dir = os.environ.get("DUPLICATE_DIR")  # relative directory to move duplicates to
+
+    # log output
+    console = bool(os.environ.get("CONSOLE"))  # output logs to console
+
     scan_path = "."  # only current work directory supported
+
     # logging
     log = Log(path=uploaded_dir)
     mkdir(uploaded_dir)
@@ -143,5 +174,13 @@ if __name__ == "__main__":
     # start the work
     files = scan_dir(scan_path, prefixes, types)
     files, duplicates = split_duplicates(files)
-    upload_move_files(files, url, data, file_descriptor, uploaded_dir)
-    clean_move_files(duplicates, duplicate_dir)
+    if len(files):
+        msg = f"Found {len(files)} files, uploading..."
+        log.append(msg)
+        print(msg)
+        upload_move_files(files, url, data, file_descriptor, uploaded_dir)
+    if len(duplicates):
+        msg = f"Found {len(duplicates)} duplicates, cleaning..."
+        log.append(msg)
+        print(msg)
+        clean_move_files(duplicates, duplicate_dir)
